e192da4a27df2a23e04e0f8c20dea1e2ff98debf
Jonathan <jonathan.corallo.utn@gmail.com>
2015-02-12 10:21:37 -0300

    Reutilizacion de conexiones

--- app/src/main/java/lolawebshop/photouploader/MainActivity.java
+++ app/src/main/java/lolawebshop/photouploader/MainActivity.java
@@ -6,13 +6,17 @@ package lolawebshop.photouploader;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
+import android.util.Log;
 
 public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initConnectionManager();
+
         FragmentTabHost tabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
             tabHost.setup(this,
                     getSupportFragmentManager(), android.R.id.tabcontent);
@@ -22,5 +26,13 @@ public class MainActivity extends FragmentActivity {
                     Modificar.class, null);
             tabHost.addTab(tabHost.newTabSpec("tab3").setIndicator("Eliminar"),
                     Eliminar.class, null);
+    }
+
+    private void initConnectionManager() {
+        Log.i("Background Thread", "init task");
+        ConnectionManager task = ConnectionManager.getInstance();
+        task.setActivity(this);
+        task.execute();
+        Log.i("Background Thread", "executing task");
     }
 }
